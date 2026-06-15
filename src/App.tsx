9f21719a4e7b66686d9a15d72e51c636ec13f245
RAUDY <raudy.web@gmail.com>
2026-06-15 13:18:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import About from "./pages/About.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import ScrollToHash from "./components/ScrollToHash.tsx";
 
 const App = () => (
   <BrowserRouter>
+    <ScrollToHash />
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/sobre-nosotros" element={<About />} />
@@ -12,4 +14,5 @@ const App = () => (
     </Routes>
   </BrowserRouter>
 );
+
 export default App;

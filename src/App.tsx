4b33de4d1245de0f4ff7e3b00d2dec24a14ccce1
RAUDY <raudy.web@gmail.com>
2026-06-19 13:17:23 +0200

    Replace Carta route with PDF link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import About from "./pages/About.tsx";
-import Carta from "./pages/Carta.tsx";
 import FoodTruck from "./pages/FoodTruck.tsx";
 import MacuBakery from "./pages/MacuBakery.tsx";
 import NotFound from "./pages/NotFound.tsx";
@@ -11,11 +10,15 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/sobre-nosotros" element={<About />} />
-      <Route path="/carta" element={<Carta />} />
+      <Route 
+        path="/carta" 
+        element={<a href="/carta.pdf" target="_blank" rel="noopener noreferrer" />}
+      />
       <Route path="/food-truck" element={<FoodTruck />} />
       <Route path="/macu-bakery" element={<MacuBakery />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
+
 export default App;

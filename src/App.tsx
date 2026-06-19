7200e7cd4a98bddbf70ffa0f09ac1d60078b33fe
RAUDY <raudy.web@gmail.com>
2026-06-19 12:57:52 +0200

    Add new routes for Carta, FoodTruck, and MacuBakery

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import About from "./pages/About.tsx";
+import Carta from "./pages/Carta.tsx";
+import FoodTruck from "./pages/FoodTruck.tsx";
+import MacuBakery from "./pages/MacuBakery.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import ScrollToHash from "./components/ScrollToHash.tsx";
 
 const App = () => (
   <BrowserRouter>
-    <ScrollToHash />
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/sobre-nosotros" element={<About />} />
+      <Route path="/carta" element={<Carta />} />
+      <Route path="/food-truck" element={<FoodTruck />} />
+      <Route path="/macu-bakery" element={<MacuBakery />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
-
 export default App;

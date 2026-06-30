8618a91a572add278b7e038edc42b78a43b21033
RAUDY <raudy.web@gmail.com>
2026-06-30 02:01:15 +0200

    Wrap App component with HelmetProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 // Developed by RAUDY
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);

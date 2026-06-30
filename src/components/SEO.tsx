ba1ef4553bb377d25ca6ce83d6faa3d5bc9d68d2
RAUDY <raudy.web@gmail.com>
2026-06-30 02:01:51 +0200

    Add SEO component with title and description

---
+++ src/components/SEO.tsx
@@ -0,0 +1,4 @@
+<SEO
+  title="Restart Coffee | Café de especialidad y brunch en Vilanova i la Geltrú"
+  description="Disfruta de café de especialidad, brunch y repostería artesanal en Restart Coffee."
+/>
